75dddc7cd6068009b3510a6af65079aeb57828c2
Bridgette1212-latu <147478576+Bridgette1212-latu@users.noreply.github.com>
2025-06-30 22:48:43 +0700

    Add files via upload

---
+++ service-wolker.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = "travelmate-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/main.js",
+  "/assets/header.png",
+  "/assets/destination-1.jpg",
+  "/assets/destination-2.jpg",
+  "/assets/destination-3.jpg",
+  "/assets/showcase.jpg",
+  "/assets/client-1.jpg",
+  "/assets/client-2.jpg",
+  "/assets/client-3.jpg",
+  "/assets/header-bg.jpg",
+  "/assets/icon.png",
+  "/manifest.json",
+
+  "https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css",
+  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css",
+  "https://unpkg.com/scrollreveal",
+  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
+  "https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // IMPORTANT: Clone the response. A response is a stream
+        // and can only be consumed once. We must clone it so that
+        // we can consume one in the cache and one in the browser.
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
